c3f4885d45c6b1c4de05e6dec67fa9fe8e13dc58
taiseiue <taisei@wsoft.ws>
2023-06-28 21:13:32 +0900

    テストビルド

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Welcome to WSOFT's WebSite!"
+echo "Copyright © 2023 WSOFT All Rights Reserved."
+
+chmod +x cei
+
+echo "Building the WSOFT's WebSite..."
+cei .
+echo "Build completed. Please check . directory."
